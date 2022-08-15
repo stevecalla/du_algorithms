8deac21078d639ac9e3a1f0ff8a1da1299524747
Steve Calla <callasteven@gmail.com>
2022-08-15 13:45:49 -0600

    Add week 10 algo

--- 9_week/03-Algorithms/02-product-of-largest-two/Unsolved/product-of-largest-two.js
+++ 9_week/03-Algorithms/02-product-of-largest-two/Unsolved/product-of-largest-two.js
@@ -1,24 +1,58 @@
 // Write code to create a function that accepts an array of numbers, finds the largest two numbers, and returns the product of the two
 
 var productOfLargestTwo = function(arr) {
-  // let largetTwo = [];
-  let previousElement = 0;
-  let largest = 0;
-  let secondLargest = 0;
+// var test = function() {
 
-  arr.forEach(element => {
-    if (element > largest) {largest = element}
+  // let arr = [20, 10, 30, 6, 3];
+  // let arr = [4, 8, 15, 16, 23, 42];
+  // let arr = [9, 8, 16, 42, 23, 15];
+  // let arr = [-10, -5, -2, -15, -1, -33, -88, -100];
+  console.log(arr);
 
-    
-    previousElement = element
+  arr.sort(function (a, b) {
+    return a - b;
+  });
+  
+  largest = arr[arr.length - 1];
+  secondLargest = arr[arr.length - 2];
+  
+  console.log(arr);
+  console.log(largest, secondLargest, largest * secondLargest);
 
-    console.log('e = ', element, 'l = ', largest, '2nd = ', secondLargest, 'prior = ', previousElement);
-    
-    if (previousElement < largest) {secondLargest = previousElement};
-
-  })
-
-  console.log(largest, secondLargest, largest * secondLargest)
-  // return largest * secondLargest;
+  return largest * secondLargest;
 
 };
+
+// var productOfLargestTwo = function(arr) {
+  var test = function(arr) {
+  
+    // let arr = [20, 10, 30, 6, 3];
+    // let arr = [4, 8, 15, 16, 23, 42];
+    // let arr = [9, 8, 16, 42, 23, 15];
+    // let arr = [-10, -5, -2, -15, -1, -33, -88, -100];
+    console.log(arr);
+  
+    arr.sort(function (a, b) {
+      return a - b;
+    });
+    
+    largest = arr[arr.length - 1];
+    secondLargest = arr[arr.length - 2];
+    
+    console.log(arr);
+    console.log(largest, secondLargest, largest * secondLargest);
+  
+    return largest * secondLargest;
+  
+  };
+
+  let arr1 = [20, 10, 30, 6, 3];
+  let arr2 = [4, 8, 15, 16, 23, 42];
+  let arr3 = [9, 8, 16, 42, 23, 15];
+  let arr4 = [-10, -5, -2, -15, -1, -33, -88, -100];
+
+  test(arr1);
+  test(arr2);
+  test(arr3);
+  test(arr4);
+
